92d63c1b8988d633c6b9eea88d9ab6b06f0223cb
KevinCayo10 <kcayo6155@uta.edu.ec>
2022-06-12 21:42:32 -0500

    "Añadi mi nombre Kevin Cayo"

--- src/proyecto1cv/Proyecto1CV.java
+++ src/proyecto1cv/Proyecto1CV.java
@@ -16,6 +16,7 @@ public class Proyecto1CV {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Kevin Cayo");
     }
     
 }
