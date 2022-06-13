3445ec3906a6be9db6596936dfb48cbb6d4cc6a1
Alienware <Alienware@DESKTOP-QUM8FSG>
2022-06-12 21:48:13 -0500

    "Es mi nombre"

--- src/proyecto1cv/Proyecto1CV.java
+++ src/proyecto1cv/Proyecto1CV.java
@@ -17,6 +17,7 @@ public class Proyecto1CV {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Kevin Cayo");
+        System.out.println("Edison Sanchez");
     }
     
 }
