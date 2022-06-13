2d91473b2653a0229bd8000c18d9ad8d45c4eab2
KevinCayo10 <kcayo6155@uta.edu.ec>
2022-06-12 22:20:55 -0500

    "Se hizo un revert"

--- src/proyecto1cv/Proyecto1CV.java
+++ src/proyecto1cv/Proyecto1CV.java
@@ -16,9 +16,10 @@ public class Proyecto1CV {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+
         System.out.println("Kevin Cayo");
         System.out.println("Edison Sanchez");
-        System.out.println("Rene teran");
+  
     }
     
 }
